f54c0abf701881c56719ad7a8390f8642397a1d2
MarikaEnslin <88105089+MarikaEnslin@users.noreply.github.com>
2021-11-28 14:41:46 +0200

    Initial commit
    Initial commit for the Netflix clone

---
+++ js/netflix.js
@@ -0,0 +1,9 @@
+class movie{
+    constructor(name, author, imagePath, trailerPath) {
+        this._name = name;
+        this._author = author;
+        this._imagePath = imagePath;
+        this._trailerPath = trailerPath;
+    }    
+    
+}
